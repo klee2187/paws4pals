40ff8f22da0d270d905dd4e38fb81fefb443692c
klee2187 <kkelly6@byui.edu>
2026-04-01 01:12:32 -0600

    image refactoring and worked a bit on homepage

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
-loadHeaderFooter();
+function qs(selector) {
+    return document.querySelector(selector);
+}
+
+loadHeaderFooter().then(() => {
+    const toggle = qs("#menu-toggle");
+    const menu = qs("#menu");
+
+    toggle.addEventListener("click", () => {
+        menu.classList.toggle("open");
+    });
+});
+
